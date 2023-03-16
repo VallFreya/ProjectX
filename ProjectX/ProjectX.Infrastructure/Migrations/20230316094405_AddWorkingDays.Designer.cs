976ffc8644c0ea04d96035f6749f271551bc3ff0
Valentina <valechka2301@gmail.com>
2023-03-16 13:01:00 +0300

    add working days table

--- ProjectX/ProjectX.Infrastructure/Migrations/20230316094405_AddWorkingDays.Designer.cs
+++ ProjectX/ProjectX.Infrastructure/Migrations/20230316094405_AddWorkingDays.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectX.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using ProjectX.Infrastructure.Data;
 namespace ProjectX.Infrastructure.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230316094405_AddWorkingDays")]
+    partial class AddWorkingDays
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProjectX/ProjectX.Infrastructure/Migrations/20230316094405_AddWorkingDays.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectX.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorkingDays : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkingDays",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MasterId = table.Column<int>(type: "int", nullable: false),
+                    WorkDay = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkingDays", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkingDays_Masters_MasterId",
+                        column: x => x.MasterId,
+                        principalTable: "Masters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkingDays_MasterId",
+                table: "WorkingDays",
+                column: "MasterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkingDays");
+        }
+    }
+}
