6a1fc4859720e91e839a2f5ad0cb5e6e4e2d17b8
gorbacheva.diana.alekseevna <dexpvallfreya@gmail.com>
2023-03-27 13:16:16 +0300

    Add enum MasterTemplateType

---
+++ ProjectX/ProjectX.Core/Entities/Enums/MasterTemplateType.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjectX.Core.Entities.Enums
+{
+    /// <summary>
+    /// Тип шаблона расписания
+    /// </summary>
+    enum MasterTemplateType
+    {
+        /// <summary>
+        /// Шаблон на будний день
+        /// </summary>
+        WeekDay = 1,
+
+        /// <summary>
+        /// Шаблон на выходной день
+        /// </summary>
+        WeekEnd = 2,
+
+        /// <summary>
+        /// Шаблон не подходящий под критерии выходного или буднего дня
+        /// </summary>
+        Custom = 3
+    }
+}
